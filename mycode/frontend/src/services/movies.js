f274f51bac2df11671879915e72d698751fc4d00
marvek16 <marvelousk1603f@gmail.com>
2025-07-03 10:19:17 +0200

    commit

--- mycode/frontend/src/services/movies.js
+++ mycode/frontend/src/services/movies.js
@@ -2,35 +2,35 @@ import axios from "axios";
 
 class MovieDataService {
     getAll(page = 0) {
-        return axios.get(`http://localhost:5000/api/v1/movies?page=${page}`)
+        return axios.get(`https://mern-backend-oqbt.onrender.com/api/v1/movies?page=${page}`)
     }
     get(id) {
-        return axios.get(`http://localhost:5000/api/v1/movies/id/${id}`)
+        return axios.get(`https://mern-backend-oqbt.onrender.com/api/v1/movies/id/${id}`)
     }
     // find(title) {
     //     return axios.get(
-    //         `http://localhost:5000/api/v1/movies?title=${title}`
+    //         `https://mern-backend-oqbt.onrender.com/api/v1/movies?title=${title}`
     //     )
     // }
     find(query, by = "title", page = 0, rating) {
         return axios.get(
-            `http://localhost:5000/api/v1/movies?${by}=${query}&page=${page}&rating=${rating}`
+            `https://mern-backend-oqbt.onrender.com/api/v1/movies?${by}=${query}&page=${page}&rating=${rating}`
         )
     }
     createReview(data) {
-        return axios.post("http://localhost:5000/api/v1/movies/review", data)
+        return axios.post("https://mern-backend-oqbt.onrender.com/api/v1/movies/review", data)
     }
     updateReview(data) {
-        return axios.put("http://localhost:5000/api/v1/movies/review", data)
+        return axios.put("https://mern-backend-oqbt.onrender.com/api/v1/movies/review", data)
     }
     deleteReview(id, userId) {
         return axios.delete(
-            "http://localhost:5000/api/v1/movies/review",
+            "https://mern-backend-oqbt.onrender.com/api/v1/movies/review",
             { data: { review_id: id, user_id: userId } }
         )
     }
     getRatings() {
-        return axios.get("http://localhost:5000/api/v1/movies/ratings")
+        return axios.get("https://mern-backend-oqbt.onrender.com/api/v1/movies/ratings")
     }
 }
 
