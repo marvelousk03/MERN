e0b20131e65b74d75d904c9623233e4eeb0db830
marvek16 <marvelousk1603f@gmail.com>
2025-07-10 18:22:51 +0200

    commit

--- user-auth/client/src/App.js
+++ user-auth/client/src/App.js
@@ -1,8 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import { Login, Signup } from "./pages";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   );
 }
