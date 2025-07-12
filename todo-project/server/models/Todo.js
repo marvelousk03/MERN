9ffa3efecf5b698e28c529198716fbe486dd08cc
marvek16 <marvelousk1603f@gmail.com>
2025-07-12 22:32:40 +0200

    commit

--- todo-project/server/models/Todo.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const todoSchema = new mongoose.Schema({
-  task: String,
-  completed: Boolean,
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-});
-
-export default mongoose.model("Todo", todoSchema);
